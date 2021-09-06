d178e618702ea36ac32cc3dc28bfebbc9d35149f
mamume <mmustafametwally@gmail.com>
2021-09-06 15:45:00 +0200

    Add Comments

--- js-labs/lab6/fetch/index.js
+++ js-labs/lab6/fetch/index.js
@@ -1,24 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Fetch the api url
     fetch('https://reqres.in/api/users?page=2')
         .then(response => response.json())
         .then(obj => {
-            console.log(obj.data);
+            // Get data from object
             const data = obj.data;
 
+            // Select profiles container
             const profiles = document.querySelector('#profiles');
 
+            // Create profile div for each user in data
             data.forEach(user => {
+                // Create div element for email
                 const email = document.createElement('div');
                 email.innerText = user.email;
 
+                // Create img element for avatar
                 const avatar = document.createElement('img');
-                // avatar.className = 'img';
                 avatar.src = user.avatar;
 
+                // Create profile div to contain avtar and email elements
                 const profile = document.createElement('div');
                 profile.className = 'profile';
                 profile.append(avatar, email);
 
+                // Append profile div in profiles container
                 profiles.appendChild(profile);
             });
         });
--- js-labs/lab6/login_page/login.js
+++ js-labs/lab6/login_page/login.js
@@ -1,24 +1,18 @@
 function login() {
+    // Get email from email field
     const email = document.querySelector('#email');
+    // Get password from password field
     const password = document.querySelector('#password');
 
+    // Select alert div
     const alertMessage = document.querySelector('#alert-message');
 
-    // if (username.value === 'admin' && password.value === '123') {
-    //     document.querySelector('#header').innerText = 'Logged In';
-    //     document.querySelector('table').hidden = true;
-    // } else {
-    //     document.querySelector('#alert-message').innerText = 'Not Registered';
-    //     username.value = '';
-    //     password.value = '';
-    // }
-
-    console.log(email.value, password.value);
-
+    // Check if email is empty
     if (!email.value) {
         alertMessage.innerText = 'Email is required!';
         alertMessage.style.color = 'red';
     }
+    // check if password is empty
     else if (!password.value) {
         alertMessage.innerText = 'Password is required!';
         alertMessage.style.color = 'red';
@@ -28,12 +22,15 @@ function login() {
         alertMessage.innerText = 'Email is invalid!';
         alertMessage.style.color = 'red';
     }
+    // If email and password is valid
     else {
+        // Create request body
         payload = {
             "email": email.value,
             "password": password.value
         };
 
+        // Fetch login api
         fetch('https://reqres.in/api/login', {
             method: 'POST',
             headers: {
@@ -42,21 +39,25 @@ function login() {
             body: JSON.stringify(payload)
         }).then(response => response.json())
             .then(data => {
+                // Check if token is found (login is succeeded)
                 if (data.token) {
+                    // Display success message
                     alertMessage.innerHTML = 'Successful Login!';
                     alertMessage.style.color = 'green';
+                    // Redirect to fetch page
                     setTimeout(() => (location.href = '../fetch/index.html'), 3000);
-
                 }
+                // If login not succeeded, display error
                 else {
                     alertMessage.style.color = 'red';
                     alertMessage.innerHTML = data.error;
                 }
-
             });
     }
 }
 
+// After DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
+    // Add an event listener on login button to run login function
     document.querySelector('button').addEventListener('click', login);
 });
