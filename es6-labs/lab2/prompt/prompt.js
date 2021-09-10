6909bb6e8c05b237948ea57ddc18d0e548b74a38
mamume <mmustafametwally@gmail.com>
2021-09-10 15:44:40 +0200

    es6/lab2 Final

--- es6-labs/lab2/prompt/prompt.js
+++ es6-labs/lab2/prompt/prompt.js
@@ -11,27 +11,13 @@ function customPrompt(msg) {
         const okBtn = document.querySelector('#ok-btn');
         const cancelBtn = document.querySelector('#cancel-btn');
 
-        // console.log(input, okBtn, cancelBtn);
-
         okBtn.addEventListener('click', () => res(input.value));
         cancelBtn.addEventListener('click', () => res(null));
     });
 }
 
-// function confirmPrompt(e) {
-//     // console.log(e.target);
-// }
-
-// function cancelPrompt() {
-
-// }
-
 document.addEventListener('DOMContentLoaded', async () => {
-    // document.querySelector('#cancel-btn').addEventListener('click', cancelPrompt);
-    // document.querySelector('#ok-btn').addEventListener('click', confirmPrompt);
-
     let username = await customPrompt('Your Name please?');
-    // console.log(username)
 
     document.querySelector('#username').innerText = username ? `, ${username}` : ', Friend';
     document.querySelector('#prompt').style.display = 'none';
