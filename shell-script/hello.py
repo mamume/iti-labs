6c1053e4b60714056f997b9a6b640fc5dc7d1135
mamume <mmustafametwally@gmail.com>
2021-10-11 14:41:11 +0200

    edit hello.py

--- shell-script/hello.py
+++ shell-script/hello.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import cgi
-# HTML is following  
-print("Content-Type: text/html")     
-# blank line, end of headers  
-print()   
-print("<title>CGI Script</title>")  
-print("<h1>This is my first CGI script</h1>")  
-print("<br>Hello, World!<br>")  
+# HTML is following
+print("Content-Type: text/html")
+# blank line, end of headers
+print()
+print("<title>CGI Script</title>")
+print("<h1>This is my first CGI script</h1>")
+print("Hello, World!<br>")
