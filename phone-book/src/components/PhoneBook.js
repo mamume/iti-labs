9f11f4cc357bdfd55ed95256f61ddd26da647950
mamume <mmustafametwally@gmail.com>
2021-10-29 19:34:24 +0200

    Adding dumy contact data

--- phone-book/src/components/PhoneBook.js
+++ phone-book/src/components/PhoneBook.js
@@ -1,9 +1,27 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import nextId from 'react-id-generator'
 
 
 function PhoneBook() {
     const [contacts, setContacts] = useState([
-        {}
+        {
+            id: nextId(),
+            name: 'Mahmoud',
+            Address: 'Add1',
+            pnumber: '0100200300'
+        },
+        {
+            id: nextId(),
+            name: 'Mustafa',
+            Address: 'Add2',
+            pnumber: '0100400500'
+        },
+        {
+            id: nextId(),
+            name: 'Metwally',
+            Address: 'Add2',
+            pnumber: '0100600700'
+        }
     ])
 
     return ('Hello, World!');
