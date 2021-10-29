c42306cb5d2ad91f1c3655a8bf7f5b31104e510b
mamume <mmustafametwally@gmail.com>
2021-10-29 15:56:18 +0200

    Creating Components

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+
 function Todo() {
-    return ("hi");
+
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+        </Container>
+    )
 }
 
 export default Todo;
