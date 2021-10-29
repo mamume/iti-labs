f8bc7e7aca02f5c13971d3bc8a4182e0599dd4f2
mamume <mmustafametwally@gmail.com>
2021-10-29 16:05:49 +0200

    Adding default todos

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
+import nextId from "react-id-generator";
+import TodoItem from './TodoItem'
 
 
 function Todo() {
+    const [todos, setTodos] = useState([
+        {
+            id: nextId(),
+            title: 'Read a book',
+            done: false,
+        },
+        {
+            id: nextId(),
+            title: 'Watch a movie',
+            done: false,
+        },
+        {
+            id: nextId(),
+            title: 'Play a game',
+            done: false,
+        }
+    ])
 
     return (
         <Container maxWidth="sm">
-            <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+            <h1>Todo List</h1>
+            {todos.map(todo => <TodoItem todo=todo />)}
         </Container>
     )
 }
