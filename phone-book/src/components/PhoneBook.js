beb5bab0d231967311e0c426e920d2ad60b79768
mamume <mmustafametwally@gmail.com>
2021-10-30 13:53:13 +0200

    Last Touches

--- phone-book/src/components/PhoneBook.js
+++ phone-book/src/components/PhoneBook.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import nextId from 'react-id-generator'
 import Container from '@mui/material/Container';
 import DisplayData from './DisplayData'
