be4c9da88de33cc0e1858ed3a433d6a02be5fb93
mamume <mmustafametwally@gmail.com>
2021-11-02 09:55:48 +0200

    Add redirect to login

--- webapp/src/components/login-register/login.js
+++ webapp/src/components/login-register/login.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import LoginBtn from "./LoginBtn";
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 
 function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    // const [token, setToken] = useState('')
+    const [token, setToken] = useState('')
     const [message, setMessage] = useState('')
 
     // "email": "eve.holt@reqres.in",
@@ -35,49 +35,55 @@ function Login() {
                 .then(data => {
                     if (data.error)
                         setMessage(data.error)
-                    else
+                    else {
                         setMessage(`Login is Successful. Token: ${data.token}`)
+                        setTimeout(setToken(data.token), 10000)
+                    }
                 })
     }
 
 
     return (
-        // Login Form
-        <form className="p-4 border mx-auto mt-lg-5 rounded container w-50">
-            {/* Alert Message Div */}
-            {message &&
-                <div id='alert-message' className="text-center alert alert-primary" role="alert">
-                    {message}
-                </div>}
+        <Fragment>
+            {token
+                ? <Redirect to="/Todo" />
+                : // Login Form
+                <form className="p-4 border mx-auto mt-lg-5 rounded container w-50">
+                    {/* Alert Message Div */}
+                    {message &&
+                        <div id='alert-message' className="text-center alert alert-primary" role="alert">
+                            {message}
+                        </div>}
 
-            {/* Login by Facebook or Google buttons */}
-            <div className="d-grid gap-2">
-                <LoginBtn name="Facebook" color="#297BE5" />
-                <LoginBtn name="Twitter" color="#1C9CEA" />
-                <LoginBtn name="Google" color="#34A853" />
-            </div>
+                    {/* Login by Facebook or Google buttons */}
+                    <div className="d-grid gap-2">
+                        <LoginBtn name="Facebook" color="#297BE5" />
+                        <LoginBtn name="Twitter" color="#1C9CEA" />
+                        <LoginBtn name="Google" color="#34A853" />
+                    </div>
 
-            <div className="form-text text-center mt-3">OR</div>
+                    <div className="form-text text-center mt-3">OR</div>
 
-            {/* Login Fields */}
-            <div className="my-2">
-                <label htmlFor="email" className="form-label">Email address</label>
-                <input id="email" type="email" className="form-control" aria-describedby="emailHelp" value={email} onChange={e => setEmail(e.target.value)} />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="password" className="form-label">Password</label>
-                <input id="password" type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} />
-            </div>
-            <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="checkbox" />
-                <label className="form-check-label" htmlFor="checkbox">Remember Me</label>
-            </div>
-            <button type="submit" className="btn btn-primary w-100" id='submit-btn' onClick={handleLogin}>Submit</button>
+                    {/* Login Fields */}
+                    <div className="my-2">
+                        <label htmlFor="email" className="form-label">Email address</label>
+                        <input id="email" type="email" className="form-control" aria-describedby="emailHelp" value={email} onChange={e => setEmail(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input id="password" type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="checkbox" />
+                        <label className="form-check-label" htmlFor="checkbox">Remember Me</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary w-100" id='submit-btn' onClick={handleLogin}>Submit</button>
 
-            <div className="form-text mt-lg-3">
-                No account yet? <Link to="/Register">Register Here</Link>
-            </div>
-        </form>
+                    <div className="form-text mt-lg-3">
+                        No account yet? <Link to="/Register">Register Here</Link>
+                    </div>
+                </form>}
+        </Fragment>
     );
 }
 
