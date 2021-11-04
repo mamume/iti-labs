cc7920c0d2de4374c4358a21d7ead97f696dc0db
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-11-04 15:22:00 +0200

    Renaming Project, adding dummy data

--- iti_django/lab1/settings.py
+++ iti_django/lab1/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'todo/templates')
+            # os.path.join(BASE_DIR, 'todo/templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- iti_django/todo/views.py
+++ iti_django/todo/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+todos = [
+    {
+        id: 1,
+        title: 'Read a book',
+        priority: 1
+    },
+    {
+        id: 2,
+        title: 'Play a game',
+        priority: 3
+    },
+    {
+        id: 3,
+        title: 'Watch a movie',
+        priority: 5
+    }
+]
+
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello, World!")
