0855908228f6267da5cc25256acf5910521c1013
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-11-04 15:49:23 +0200

    Adding bootstrap

--- iti_django/todo/urls.py
+++ iti_django/todo/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, view, edit, delete
 
 app_name = 'todo'
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('view/<int:todo_id>', view, name='view'),
+    path('edit/<int:todo_id>', edit, name='edit'),
+    path('delete/<int:todo_id>', delete, name='delete')
 ]
--- iti_django/todo/views.py
+++ iti_django/todo/views.py
@@ -3,23 +3,39 @@ from django.http import HttpResponse
 
 todos = [
     {
-        id: 1,
-        title: 'Read a book',
-        priority: 1
+        "id": 1,
+        "title": 'Read a book',
+        "priority": 1,
+        "description": "desc. 1: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
     },
     {
-        id: 2,
-        title: 'Play a game',
-        priority: 3
+        "id": 2,
+        "title": 'Play a game',
+        "priority": 3,
+            "description": "desc. 2: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
     },
     {
-        id: 3,
-        title: 'Watch a movie',
-        priority: 5
+        "id": 3,
+        "title": 'Watch a movie',
+        "priority": 5,
+            "description": "desc. 1: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
     }
 ]
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello, World!")
-    return render(request, 'index.html')
+    return render(request, 'index.html', context={"todos":todos})
+
+
+def view(request, todo_id):
+
+
+    return render(request, 'index.html', context={"todos":todos})
+
+
+def edit(request, todo_id):
+    pass
+
+def delete(request, todo_id):
+    pass
