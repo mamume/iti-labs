230f53f574e8f8cf50e0bec00756ff3702e819f6
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-11-06 14:30:19 +0200

    Sign up Finished

--- cinema/account/urls.py
+++ cinema/account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import login
+from .views import signup
+
+app_name = 'account'
 
 urlpatterns = [
     path('signup/', signup, name='signup')
--- cinema/account/views.py
+++ cinema/account/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup(request):
-    return render(request, 'signup.html')
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            return redirect('login')
+
+    return render(request, 'signup.html', context={'form': form})
