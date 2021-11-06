c1b745b7c1eb2da4904dad7099a11c41b5c6e99c
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-11-06 15:11:27 +0200

    Admin customization finished

--- cinema/moviedb/admin.py
+++ cinema/moviedb/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from moviedb.models import Movie, Category, Actor, Director, ActorID
 
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'rating', 'director', 'list_actors')
+    search_fields = ('name', 'rating')
+    ordering = ['name']
+
+    def list_actors(self, obj):
+        actors = obj.cast.all()
+        name = ''
+        for actor in actors:
+            name += actor.name + ' '
+        return name
+
 # Register your models here.
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
 admin.site.register(Category)
 admin.site.register(Actor)
 admin.site.register(Director)
