8f04e6f1a08e7a6da36be1ee28264188c9300ebe
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-11-06 13:29:13 +0200

    Adding media path

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -132,3 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media_root')
