09dab9eb2aa46c29b334edfd4727c0132ebd984e
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-11-06 14:55:08 +0200

    Relations Finished

--- cinema/moviedb/admin.py
+++ cinema/moviedb/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from moviedb.models import Movie, Category, Actor
+from moviedb.models import Movie, Category, Actor, Director, ActorID
 
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(Category)
 admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(ActorID)
--- cinema/moviedb/models.py
+++ cinema/moviedb/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
 
+class ActorID(models.Model):
+    idNum = models.IntegerField()
+
+    def __str__(self):
+        return str(self.idNum)
+
+class Director(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
@@ -10,6 +23,7 @@ class Category(models.Model):
 class Actor(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
+    idnum = models.OneToOneField(ActorID, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
@@ -21,3 +35,4 @@ class Movie(models.Model):
     rating = models.IntegerField(default=0, null=True, blank=True, validators=[MaxValueValidator(10)])
     category = models.ManyToManyField(Category)
     cast = models.ManyToManyField(Actor)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True, blank=True)
