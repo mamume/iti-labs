42b7477315ace9a96fb700e9fbdd9979399d9eae
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-11-12 14:27:49 +0200

    List Movies Finished

--- lab4/multimedia/api/v1/urls.py
+++ lab4/multimedia/api/v1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_movie
+from .views import create_movie, movies_details
 
 app_name = 'multimedia'
 
 urlpatterns = [
-    path('create', create_movie, name="create_movie")
+    path('create', create_movie, name="create_movie"),
+    path('details', movies_details, name="movies_details")
 ]
--- lab4/multimedia/api/v1/views.py
+++ lab4/multimedia/api/v1/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import MovieSerializer
+from multimedia.models import Movie
 
 @api_view(["POST"])
 def create_movie(request):
@@ -19,3 +20,10 @@ def create_movie(request):
         return Response(**response)
     else:
         return Response(data=serialized_movie.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET"])
+def movies_details(request):
+    all_movies = Movie.objects.all()
+    serialized_movies = MovieSerializer(instance=all_movies, many=True)
+
+    return Response(data=serialized_movies.data, status=status.HTTP_200_OK)
