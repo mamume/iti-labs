c2c34a7df49466011b99100d89b7e9670ee84211
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-11-12 16:28:58 +0200

    Almost Finished

--- lab4/multimedia/api/v1/serializers.py
+++ lab4/multimedia/api/v1/serializers.py
@@ -1,13 +1,38 @@
 from rest_framework import serializers
-from multimedia.models import Movie, Serie, Category
+from multimedia.models import Movie, Serie, Category, Actor
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ['name']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
 
 class MovieSerializer(serializers.ModelSerializer):
+    # casts = ActorSerializer('casts', many=True)
+    # categories = CategorySerializer('categories', many=True)
+    
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = ('__all__')
+        # read_only_fields = ('casts', 'categories')
+        depth = 1
+
+    # def list_actors(self, obj):
+    #     actors = obj.casts.all()
+    #     name = ''
+    #     for actor in actors:
+    #         name += actor.name + ' '
+    #     return name
 
 class SerieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Serie
         fields = '__all__'
+
 
