013a9022518893b763effc6851a717e22f43d163
mamume <mmustafametwally@gmail.com>
2021-11-18 13:17:11 +0200

    Basic app Finished

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import DEBUG
 from flask import Flask, request, jsonify, abort, render_template, flash
 from flask_restful import Resource, Api
 import logging
@@ -9,3 +10,6 @@ app = Flask(__name__)
 @app.route('/hello/', methods=['GET', 'POST'])
 def hello():
     return "hello"
+
+
+app.run(DEBUG=True)
