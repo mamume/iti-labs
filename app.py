b229ea1fafffe0e9957cdf306659852f37813a2a
mamume <mmustafametwally@gmail.com>
2021-11-18 13:06:22 +0200

    Update .gitignore

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ from flask_restful import Resource, Api
 import logging
 
 logging.basicConfig(filename='flask.logs')
+
+app = Flask(__name__)
+
+@app.route('/hello/', methods=['GET', 'POST'])
+def hello():
+    return "hello"
