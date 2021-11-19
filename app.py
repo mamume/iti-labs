44e64c17c6106c6028779b29401dbc1a15fad49a
mamume <mmustafametwally@gmail.com>
2021-11-19 18:18:12 +0200

    Delete finished

--- app.py
+++ app.py
@@ -12,21 +12,33 @@ todo_api = Api(todo_app)
 
 class TodoRUD(Resource):
     def get(self, **kwargs):
-        todo_id = kwargs.get('todo_id')
-        task = Todo.query.get(todo_id)
+        id = kwargs.get('id')
+        todo = Todo.query.get(id)
 
-        if not task:
-            abort(404, message='Not Found')
+        if not todo:
+            abort(404, message='id is not found')
 
         data = {
-            'id': task.id,
-            'title': task.title,
-            'description': task.description,
-            'priority': task.priority,
-            'finished': task.finished
+            'id': todo.id,
+            'title': todo.title,
+            'description': todo.description,
+            'priority': todo.priority,
+            'completed': todo.completed
         }
 
         return data, 200
+
+    def delete(self, *args, **kwargs):
+        id = kwargs.get("id")
+        todo = Todo.query.get(id)
+
+        if not todo:
+            abort(404, message="id is not found")
+
+        db.session.delete(todo)
+        db.session.commit()
+
+        return {'response': f'todo with id={id} is deleted successfully'}
 
 
 todo_app.run(DEBUG=True)
